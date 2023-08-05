492515e4d9d9cfd0ef926b870a8e5fee451fff26
Hà Huy Hoàng <101927338+3hJalter@users.noreply.github.com>
2023-08-05 19:22:46 +0700

    Add controller and Stair script

---
+++ Assets/Game/Scripts/InGame/Character/AgentMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using CnControls;
+public class AgentMovement : MonoBehaviour
+{
+    [SerializeField] private Transform model;
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    [SerializeField] private float maxZPositionMove = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var input = new Vector2(CnInputManager.GetAxis("Horizontal"), CnInputManager.GetAxis("Vertical"));
+        if (input.magnitude < 0) return;
+        if (Mathf.Abs(input.y) > 0.01f)
+            Move(input);
+    }
+
+    [SerializeField] private float speed = 1f;
+    private void Move(Vector2 input)
+    {
+        var thisTransform = transform;
+        var destination = thisTransform.position + 
+                          thisTransform.right * (input.x * speed) + 
+                          thisTransform.forward * (input.y * speed);
+        model.LookAt(destination);
+        if (destination.z > maxZPositionMove) return;
+        navMeshAgent.destination = destination;
+    }
+}
